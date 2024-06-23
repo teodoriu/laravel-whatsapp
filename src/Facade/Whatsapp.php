22a89851edb5db0321bb4ca4a15c52ea604a2e5f
Teodor B <teodoriu@pm.me>
2024-06-23 14:05:22 +0300

    Update Whatsapp.php

--- src/Facade/Whatsapp.php
+++ src/Facade/Whatsapp.php
@@ -2,23 +2,26 @@
 
 namespace Teodoriu\Whatsapp\Facade;
 
+use Teodoriu\Whatsapp\BusinessProfile;
+use Teodoriu\Whatsapp\Messages\WhatsappMessage;
 use Teodoriu\Whatsapp\Whatsapp as ConcreteWhatsapp;
 use Illuminate\Support\Facades\Facade;
+use Teodoriu\Whatsapp\WhatsappMedia;
 
 /**
- * @method static array send(string|array<string> $phones, \Teodoriu\Whatsapp\WhatsappMessage $message)
- * @method static \Teodoriu\Whatsapp\Whatsapp client(string $numberId, string $token)
- * @method static \Teodoriu\Whatsapp\Whatsapp token(string $token)
- * @method static \Teodoriu\Whatsapp\Whatsapp numberId(string $numberId)
- * @method static \Teodoriu\Whatsapp\Whatsapp numberName(string $name)
- * @method static \Teodoriu\Whatsapp\Whatsapp defaultNumber()
+ * @method static array send(string|array $phones, WhatsappMessage $message)
+ * @method static ConcreteWhatsapp client(string $numberId, string $token)
+ * @method static ConcreteWhatsapp token(string $token)
+ * @method static ConcreteWhatsapp numberId(string $numberId)
+ * @method static ConcreteWhatsapp numberName(string $name)
+ * @method static ConcreteWhatsapp defaultNumber()
  * @method static bool markRead(string $messageId)
- * @method static \Teodoriu\Whatsapp\WhatsappMedia|string uploadMedia(string $file, string $type = null, bool $retrieveAllData = true)
- * @method static \Teodoriu\Whatsapp\WhatsappMedia getMedia(string $mediaId, bool $download = false)
- * @method static bool deleteMedia(\Teodoriu\Whatsapp\WhatsappMedia|string $id)
- * @method static \Teodoriu\Whatsapp\WhatsappMedia downloadMedia(string|\Teodoriu\Whatsapp\WhatsappMedia $media)
- * @method static \Teodoriu\Whatsapp\BusinessProfile getProfile()
- * @method static bool updateProfile(array|\Teodoriu\Whatsapp\BusinessProfile $data)
+ * @method static WhatsappMedia|string uploadMedia(string $file, string $type = null, bool $retrieveAllData = true)
+ * @method static WhatsappMedia getMedia(string $mediaId, bool $download = false)
+ * @method static bool deleteMedia(WhatsappMedia|string $id)
+ * @method static WhatsappMedia downloadMedia(string|WhatsappMedia $media)
+ * @method static BusinessProfile getProfile()
+ * @method static bool updateProfile(array|BusinessProfile $data)
  */
 class Whatsapp extends Facade
 {
@@ -27,7 +30,7 @@ class Whatsapp extends Facade
      *
      * @return string
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'whatsapp';
     }
